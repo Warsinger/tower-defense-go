1f26e0049f59aa4ab844fa593a8d2771876fcbea
Matt Lee <eelwehttam@gmail.com>
2024-08-12 18:06:10 -0500

    working on configui

--- scenes/title.go
+++ scenes/title.go
@@ -2,12 +2,18 @@ package scenes
 
 import (
 	"fmt"
+	"image/color"
+
 	"tower-defense/assets"
 	comp "tower-defense/components"
 
 	"github.com/hajimehoshi/ebiten/v2"
-	"github.com/hajimehoshi/ebiten/v2/inpututil"
 	"github.com/hajimehoshi/ebiten/v2/text/v2"
+
+	"github.com/ebitenui/ebitenui"
+
+	"github.com/ebitenui/ebitenui/image"
+	"github.com/ebitenui/ebitenui/widget"
 )
 
 type TitleScene struct {
@@ -15,17 +21,78 @@ type TitleScene struct {
 	height          int
 	gameStats       *GameStats
 	newGameCallback NewGameCallback
+
+	// ui elements
+	ui *ebitenui.UI
 }
 type NewGameCallback func(broadcast bool) error
 
 func NewTitleScene(width, height int, gameStats *GameStats, newGameCallback NewGameCallback) (*TitleScene, error) {
-	return &TitleScene{width: width, height: height, gameStats: gameStats, newGameCallback: newGameCallback}, nil
+	title := &TitleScene{width: width, height: height, gameStats: gameStats, newGameCallback: newGameCallback}
+	title.ui = initUI()
+	return title, nil
+}
+
+func initUI() *ebitenui.UI {
+	ui := &ebitenui.UI{}
+	buttonImage, _ := loadButtonImage()
+	face := assets.GoFace
+	rootContainer := widget.NewContainer(
+		// the container will use a plain color as its background
+		widget.ContainerOpts.BackgroundImage(image.NewNineSliceColor(color.NRGBA{0x13, 0x1a, 0x22, 0x00})),
+
+		// the container will use a row layout to layout the textinput widgets
+		widget.ContainerOpts.Layout(widget.NewAnchorLayout(
+			widget.AnchorLayoutOpts.Padding(widget.NewInsetsSimple(20)))),
+	)
+	buttonMultiplayer := widget.NewButton(
+		// set general widget options
+		widget.ButtonOpts.WidgetOpts(
+			// instruct the container's anchor layout to center the button both horizontally and vertically
+			widget.WidgetOpts.LayoutData(widget.AnchorLayoutData{
+				HorizontalPosition: widget.AnchorLayoutPositionCenter,
+				VerticalPosition:   widget.AnchorLayoutPositionEnd,
+			}),
+		),
+
+		// specify the images to use
+		widget.ButtonOpts.Image(buttonImage),
+
+		// specify the button's text, the font face, and the color
+		widget.ButtonOpts.Text("Multiplayer Options", face, &widget.ButtonTextColor{
+			Idle: color.NRGBA{0xdf, 0xf4, 0xff, 0xff},
+		}),
+
+		// specify that the button's text needs some padding for correct display
+		widget.ButtonOpts.TextPadding(widget.Insets{
+			Left:   30,
+			Right:  30,
+			Top:    5,
+			Bottom: 50,
+		}),
+
+		// add a handler that reacts to clicking the button
+		widget.ButtonOpts.ClickedHandler(func(args *widget.ButtonClickedEventArgs) {
+			// TODO start the client connection to the server
+			fmt.Printf("Loading Multiplayer options\n")
+			openWindow(ui)
+		}),
+	)
+
+	// add the button as a child of the container
+	rootContainer.AddChild(buttonMultiplayer)
+
+	ui.Container = rootContainer
+
+	return ui
 }
 
 func (t *TitleScene) Update() error {
-	if inpututil.IsMouseButtonJustPressed(ebiten.MouseButtonLeft) || ebiten.IsKeyPressed(ebiten.KeySpace) {
-		return t.newGameCallback(true)
-	}
+	t.ui.Update()
+
+	// if inpututil.IsMouseButtonJustPressed(ebiten.MouseButtonLeft) || ebiten.IsKeyPressed(ebiten.KeySpace) {
+	// 	return t.newGameCallback(true)
+	// }
 
 	return nil
 }
@@ -76,4 +143,7 @@ func (t *TitleScene) Draw(screen *ebiten.Image) {
 	nextY = 600
 	str = "Click or press space to start"
 	_ = comp.DrawTextLines(screen, assets.ScoreFace, str, width, nextY, text.AlignCenter, text.AlignStart)
+
+	// draw UI elements
+	t.ui.Draw(screen)
 }
