de55cf63ec6de0f94cb771a2ad539e3b3594d3be
Matt Lee <eelwehttam@gmail.com>
2024-08-21 21:49:31 -0500

    save and load stats

--- components/stats.go
+++ components/stats.go
@@ -1,6 +1,14 @@
 package components
 
-import "time"
+import (
+	"errors"
+	"fmt"
+	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
+	"time"
+)
 
 type GameStats struct {
 	HighScore      int
@@ -17,6 +25,7 @@ type GameStats struct {
 	CreepBulletsFired int
 	BulletsExpired    int
 	PlayerDeaths      int
+	MoneySpent        int
 	StartTime         time.Time
 	GameTime          time.Duration
 }
@@ -63,6 +72,9 @@ func (gs *GameStats) UpdateBulletsExpired() {
 func (gs *GameStats) UpdatePlayerDeaths() {
 	gs.PlayerDeaths++
 }
+func (gs *GameStats) UpdateMoneyspent(money int) {
+	gs.CreepsSpawned += money
+}
 
 func (gs *GameStats) CalcDuration() time.Duration {
 	return time.Since(gs.StartTime)
@@ -78,6 +90,96 @@ func (gs *GameStats) Reset() {
 	gs.CreepBulletsFired = 0
 	gs.BulletsExpired = 0
 	gs.PlayerDeaths = 0
+	gs.MoneySpent = 0
 	gs.StartTime = time.Now()
 	gs.GameTime = 0
+}
+
+const statsFile = "score/stats.txt"
+
+func LoadScores() *GameStats {
+	gameStats := &GameStats{}
+	bytes, err := os.ReadFile(statsFile)
+	if err == nil {
+		strings.Split(string(bytes), "\n")
+		for _, line := range strings.Split(string(bytes), "\n") {
+			values := strings.Split(line, "=")
+			if len(values) != 2 {
+				continue
+			}
+			switch values[0] {
+			case "score":
+				gameStats.HighScore = parseScore(values[0], values[1])
+			case "creepLevel":
+				gameStats.HighCreepLevel = parseScore(values[0], values[1])
+			case "towerLevel":
+				gameStats.HighTowerLevel = parseScore(values[0], values[1])
+			case "creepsSpawned":
+				gameStats.CreepsSpawned = parseScore(values[0], values[1])
+			case "creepsKilled":
+				gameStats.CreepsKilled = parseScore(values[0], values[1])
+			case "creepWaves":
+				gameStats.CreepWaves = parseScore(values[0], values[1])
+			case "towersBuilt":
+				gameStats.TowersBuilt = parseScore(values[0], values[1])
+			case "towersKilled":
+				gameStats.TowersKilled = parseScore(values[0], values[1])
+			case "towerBulletsFired":
+				gameStats.TowerBulletsFired = parseScore(values[0], values[1])
+			case "creepBulletsFired":
+				gameStats.CreepBulletsFired = parseScore(values[0], values[1])
+			case "bulletsExpired":
+				gameStats.BulletsExpired = parseScore(values[0], values[1])
+			case "playerDeaths":
+				gameStats.PlayerDeaths = parseScore(values[0], values[1])
+			case "gameTime":
+				gameStats.GameTime, err = time.ParseDuration(values[1])
+				if err != nil {
+					fmt.Printf("WARN %s formatting err %s %v\n", values[0], values[1], err)
+				}
+			}
+		}
+	}
+
+	return gameStats
+}
+
+func parseScore(label, val string) int {
+	score, err := strconv.Atoi(val)
+	if err != nil {
+		fmt.Printf("WARN %s formatting err %s %v\n", label, val, err)
+	}
+	return score
+}
+
+func (gs *GameStats) SaveScores() error {
+	dir, _ := filepath.Split(statsFile)
+
+	if err := ensureDir(dir); err != nil {
+		return err
+	}
+
+	str := fmt.Sprintf("score=%d\ncreepLevel=%d\ntowerLevel=%d\ncreepsSpawned=%d\ncreepsKilled=%d\ncreepWaves=%d\ntowersBuilt=%d\ntowersKilled=%d\ntowersAmmoOut=%d\ntowerBulletsFired=%d\ncreepbulletsFired=%d\nbulletsExpired=%d\nplayerDeaths%d\ngameTime=%v\n",
+		gs.HighScore, gs.HighCreepLevel, gs.HighTowerLevel,
+		gs.CreepsSpawned, gs.CreepsKilled, gs.CreepWaves, gs.TowersBuilt, gs.TowersKilled, gs.TowersAmmoOut, gs.TowerBulletsFired, gs.CreepBulletsFired, gs.BulletsExpired, gs.PlayerDeaths, gs.CalcDuration())
+	return os.WriteFile(statsFile, []byte(str), 0644)
+}
+
+func ensureDir(dirName string) error {
+	err := os.Mkdir(dirName, os.ModeDir)
+	if err == nil {
+		return nil
+	}
+	if os.IsExist(err) {
+		// check that the existing path is a directory
+		info, err := os.Stat(dirName)
+		if err != nil {
+			return err
+		}
+		if !info.IsDir() {
+			return errors.New("path exists but is not a directory")
+		}
+		return nil
+	}
+	return err
 }
--- game/game.go
+++ game/game.go
@@ -1,13 +1,6 @@
 package game
 
 import (
-	"errors"
-	"fmt"
-	"os"
-	"path/filepath"
-	"strconv"
-	"strings"
-	"time"
 	"tower-defense/assets"
 	"tower-defense/config"
 	"tower-defense/network"
@@ -41,7 +34,7 @@ func NewGame(width, height, speed int, startingTowerLevel int, debug, computer, 
 
 	ebiten.SetWindowTitle("Tower Defense")
 
-	gameStats := LoadScores()
+	gameStats := comp.LoadScores()
 
 	game := &GameData{world: donburi.NewWorld(), width: width, height: height, speed: speed, gameStats: gameStats, startingTowerLevel: startingTowerLevel}
 
@@ -101,7 +94,7 @@ func (g *GameData) switchToTitle(gameStats *comp.GameStats, gameOptions *config.
 		if gameStats.HighTowerLevel > g.gameStats.HighTowerLevel {
 			g.gameStats.HighTowerLevel = gameStats.HighTowerLevel
 		}
-		g.SaveScores()
+		g.gameStats.SaveScores()
 	}
 
 	title, err := scenes.NewTitleScene(g.world, g.width, g.height, g.gameStats, gameOptions, g.switchToBattle)
@@ -114,99 +107,10 @@ func (g *GameData) switchToTitle(gameStats *comp.GameStats, gameOptions *config.
 	return nil
 }
 
-const statsFile = "score/stats.txt"
-
-func LoadScores() *comp.GameStats {
-	gameStats := &comp.GameStats{}
-	bytes, err := os.ReadFile(statsFile)
-	if err == nil {
-		strings.Split(string(bytes), "\n")
-		for _, line := range strings.Split(string(bytes), "\n") {
-			values := strings.Split(line, "=")
-			if len(values) != 2 {
-				continue
-			}
-			switch values[0] {
-			case "score":
-				gameStats.HighScore = parseScore(values[0], values[1])
-			case "creepLevel":
-				gameStats.HighCreepLevel = parseScore(values[0], values[1])
-			case "towerLevel":
-				gameStats.HighTowerLevel = parseScore(values[0], values[1])
-			case "creepsSpawned":
-				gameStats.CreepsSpawned = parseScore(values[0], values[1])
-			case "creepsKilled":
-				gameStats.CreepsKilled = parseScore(values[0], values[1])
-			case "creepWaves":
-				gameStats.CreepWaves = parseScore(values[0], values[1])
-			case "towersBuilt":
-				gameStats.TowersBuilt = parseScore(values[0], values[1])
-			case "towersKilled":
-				gameStats.TowersKilled = parseScore(values[0], values[1])
-			case "towerBulletsFired":
-				gameStats.TowerBulletsFired = parseScore(values[0], values[1])
-			case "creepBulletsFired":
-				gameStats.CreepBulletsFired = parseScore(values[0], values[1])
-			case "bulletsExpired":
-				gameStats.BulletsExpired = parseScore(values[0], values[1])
-			case "playerDeaths":
-				gameStats.PlayerDeaths = parseScore(values[0], values[1])
-			case "gameTime":
-				gameStats.GameTime, err = time.ParseDuration(values[1])
-				if err != nil {
-					fmt.Printf("WARN %s formatting err %s %v\n", values[0], values[1], err)
-				}
-			}
-		}
-	}
-
-	return gameStats
-}
-
-func parseScore(label, val string) int {
-	score, err := strconv.Atoi(val)
-	if err != nil {
-		fmt.Printf("WARN %s formatting err %s %v\n", label, val, err)
-	}
-	return score
-}
-
-func (g *GameData) SaveScores() error {
-	dir, _ := filepath.Split(statsFile)
-
-	if err := ensureDir(dir); err != nil {
-		return err
-	}
-
-	str := fmt.Sprintf("score=%d\ncreepLevel=%d\ntowerLevel=%d\ncreepsSpawned=%d\ncreepsKilled=%d\ncreepWaves=%d\ntowersBuilt=%d\ntowersKilled=%d\ntowersAmmoOut=%d\ntowerBulletsFired=%d\ncreepbulletsFired=%d\nbulletsExpired=%d\nplayerDeaths%d\ngameTime=%v\n",
-		g.gameStats.HighScore, g.gameStats.HighCreepLevel, g.gameStats.HighTowerLevel,
-		g.gameStats.CreepsSpawned, g.gameStats.CreepsKilled, g.gameStats.CreepWaves, g.gameStats.TowersBuilt, g.gameStats.TowersKilled, g.gameStats.TowersAmmoOut, g.gameStats.TowerBulletsFired, g.gameStats.CreepBulletsFired, g.gameStats.BulletsExpired, g.gameStats.PlayerDeaths, g.gameStats.CalcDuration())
-	return os.WriteFile(statsFile, []byte(str), 0644)
-}
-
-func ensureDir(dirName string) error {
-	err := os.Mkdir(dirName, os.ModeDir)
-	if err == nil {
-		return nil
-	}
-	if os.IsExist(err) {
-		// check that the existing path is a directory
-		info, err := os.Stat(dirName)
-		if err != nil {
-			return err
-		}
-		if !info.IsDir() {
-			return errors.New("path exists but is not a directory")
-		}
-		return nil
-	}
-	return err
-}
-
 func (g *GameData) Update() error {
 	// TODO move this into the title scene so we can determine if the config has focus
 	if !scenes.IsModalOpen() && inpututil.IsKeyJustPressed(ebiten.KeyQ) {
-		if err := g.SaveScores(); err != nil {
+		if err := g.gameStats.SaveScores(); err != nil {
 			return err
 		}
 
